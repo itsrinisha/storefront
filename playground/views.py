51751272ff889efed0cc49467640671687d5a04d
itsrinisha <itsrinisha@gmail.com>
2023-02-12 09:43:34 +0545

    Deleting Objects

--- playground/views.py
+++ playground/views.py
@@ -1,23 +1,11 @@
 from django.shortcuts import render
-from store.models import Collection, Product
+from store.models import Collection
 
 
 def greeting(request):
-    collection = Collection(pk=11)
-    collection.title = "Makeup"
-    collection.featured_product = Product(pk=1)
-    collection.save()
+    collection = Collection(pk=1)
+    collection.delete()
 
-    collection = Collection(pk=12)
-    collection.featured_product = Product(pk=2)
-    # sets title to "", causes data loss
-    collection.save()
-
-    collection = Collection.objects.get(pk=13)
-    collection.featured_product = Product(pk=3)
-    # doesnt set title to ""
-    collection.save()
-
-    collection = Collection.objects.filter(pk=14).update(featured_product_id=4)
+    collection = Collection.objects.filter(id__gt=2).delete()
 
     return render(request, "home.html", {"name": "Nina"})

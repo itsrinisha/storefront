4607570fc5dd15296241b51187cb4b7bc5ab61d3
itsrinisha <itsrinisha@gmail.com>
2023-02-12 11:33:25 +0545

    Adding Links to Other Pages

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.db.models import Count
+from django.utils.html import format_html, urlencode
+from django.urls import reverse
 from . import models
 
 
@@ -23,10 +25,26 @@ class ProductAdmin(admin.ModelAdmin):
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ["first_name", "last_name", "membership"]
+    list_display = ["first_name", "last_name", "membership", "orders_count"]
     list_editable = ["membership"]
     list_per_page = 10
     ordering = ["first_name", "last_name"]
+
+    @admin.display(ordering="orders_count")
+    def orders_count(self, customer):
+        url = (
+            reverse("admin:store_order_changelist")
+            + "?"
+            + urlencode(
+                {"customer__id": customer.id},
+            )
+        )
+        return format_html(
+            '<a href="{}" target="_blank">{}</a>', url, customer.orders_count
+        )
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(orders_count=Count("order"))
 
 
 @admin.register(models.Order)
@@ -41,7 +59,20 @@ class CollectionAdmin(admin.ModelAdmin):
 
     @admin.display(ordering="products_count")
     def products_count(self, collection):
-        return collection.products_count
+        url = (
+            reverse("admin:store_product_changelist")
+            + "?"
+            + urlencode(
+                {
+                    "collection__id": str(collection.id),
+                }
+            )
+        )
+        return format_html(
+            '<a href="{}" target="_blank">{}</a>',
+            url,
+            collection.products_count,
+        )
 
     def get_queryset(self, request):
         return super().get_queryset(request).annotate(products_count=Count("product"))

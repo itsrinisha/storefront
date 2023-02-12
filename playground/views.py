73f544462716fd7bee1bebec18fbb9c4ff6a218f
itsrinisha <itsrinisha@gmail.com>
2023-02-12 10:36:30 +0545

    Registering Models

--- playground/views.py
+++ playground/views.py
@@ -4,14 +4,4 @@ from store.models import Product
 
 
 def greeting(request):
-    products = Product.objects.raw("SELECT id, title FROM store_product")
-    products = Product.objects.raw("SELECT * FROM store_product")
-    print(products)
-    list(products)
-
-    # Execute SQL queries not tied to a model
-    with connection.cursor() as cursor:
-        cursor.execute("INSERT ...............")
-        cursor.callproc("get_customers", [1, 2, "a"])
-
     return render(request, "home.html", {"name": "Nina"})

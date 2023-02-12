a50538eaaca9fadc4ce861de0f0eabbfd85e2fed
itsrinisha <itsrinisha@gmail.com>
2023-02-12 07:33:20 +0545

    Retrieving objects

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
+from store.models import Product
 
 
 def greeting(request):
+    # all
+    products = Product.objects.all()
+    list(products)
+
+    # get
+    # try:
+    #     product = Product.objects.get(pk=0)
+    # except ObjectDoesNotExist:
+    #     pass
+        
+    # filter
+    # product = Product.objects.filter(pk=0).first()
+
+    # exists
+    # product = Product.objects.filter(pk=0).exists()
+
+
     return render(request, "home.html", {"name": "Nina"})

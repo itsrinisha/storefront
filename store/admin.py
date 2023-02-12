c999618867363450438dccc6bc3b81921b7abc6c
itsrinisha <itsrinisha@gmail.com>
2023-02-12 11:52:12 +0545

    Customizing Forms

--- store/admin.py
+++ store/admin.py
@@ -21,6 +21,14 @@ class InventoryFilter(admin.SimpleListFilter):
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
+    # fields = ["title", "slug"]
+    # exclude = ["description"]
+    # readonly_fields = ["title"]
+    prepopulated_fields = {
+        "slug": ["title"],
+    }
+    autocomplete_fields = ["collection"]
+
     actions = ["clear_inventory"]
     list_display = ["title", "unit_price", "inventory_status", "collection_title"]
     list_editable = ["unit_price"]
@@ -77,11 +85,14 @@ class CustomerAdmin(admin.ModelAdmin):
 class OrderAdmin(admin.ModelAdmin):
     list_display = ["id", "placed_at", "customer"]
     list_select_related = ["customer"]
+    readonly_fields = ["placed_at"]
+    autocomplete_fields = ["customer"]
 
 
 @admin.register(models.Collection)
 class CollectionAdmin(admin.ModelAdmin):
     list_display = ["title", "products_count"]
+    search_fields = ["title"]
 
     @admin.display(ordering="products_count")
     def products_count(self, collection):

3dd4c053bb2118d0488be515dd5cdc7be4649322
itsrinisha <itsrinisha@gmail.com>
2023-02-12 08:09:33 +0545

    Deferring Fields

--- playground/views.py
+++ playground/views.py
@@ -1,17 +1,12 @@
 from django.shortcuts import render
-from store.models import Product, OrderItem
+from store.models import Product
 
 
 def greeting(request):
-    # queryset = Product.objects.values("id", "title", "collection__title")
-    # print(queryset)
+    queryset = Product.objects.only("id", "title", "collection__title")
+    print(queryset)
 
-    # queryset = Product.objects.values_list("id", "title", "collection__title")
-    # print(queryset)
-
-    queryset = Product.objects.filter(
-        id__in=OrderItem.objects.values("product_id").distinct()
-    ).order_by("title")
-    list(queryset)
+    queryset = Product.objects.defer("description")
+    print(queryset)
 
     return render(request, "home.html", {"name": "Nina"})

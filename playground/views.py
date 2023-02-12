dd4ed556d7726d1f97571acf77e634de2d0f4e73
itsrinisha <itsrinisha@gmail.com>
2023-02-12 09:24:19 +0545

    Custom Managers

--- playground/views.py
+++ playground/views.py
@@ -1,14 +1,9 @@
 from django.shortcuts import render
-from django.contrib.contenttypes.models import ContentType
 from store.models import Product
 from tags.models import TaggedItem
 
 
 def greeting(request):
-    content_type = ContentType.objects.get_for_model(Product)
-    queryset = TaggedItem.objects.filter(
-        content_type=content_type,
-        object_id=1,
-    )
+    queryset = TaggedItem.objects.get_tags_for(Product, 10)
     list(queryset)
     return render(request, "home.html", {"name": "Nina"})

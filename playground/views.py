638f47b0979886903eb197b16a991256ef8d2b7c
itsrinisha <itsrinisha@gmail.com>
2023-02-12 08:54:36 +0545

    Database Functions

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
-from django.db.models import Value, F
+from django.db.models.functions import Concat
+from django.db.models import Value, F, Func
 from store.models import Customer
 
 
 def greeting(request):
-    queryset = Customer.objects.annotate(is_new=Value(True))
-    queryset = Customer.objects.annotate(new_id=F('id') + 1)
+    queryset = Customer.objects.annotate(
+        full_name=Func(F("first_name"), Value(" "), F("last_name"), function="CONCAT")
+    )
+    queryset = Customer.objects.annotate(
+        full_name=Concat("first_name", Value(" "), "last_name")
+    )
     list(queryset)
     return render(request, "home.html", {"name": "Nina"})

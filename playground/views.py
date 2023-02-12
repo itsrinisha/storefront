35c20ab96b15530a36420094ff5cf21d4ff75674
itsrinisha <itsrinisha@gmail.com>
2023-02-12 09:41:33 +0545

    Updating Objects

--- playground/views.py
+++ playground/views.py
@@ -3,14 +3,21 @@ from store.models import Collection, Product
 
 
 def greeting(request):
-    collection = Collection()
-    collection.title = "Beauty"
+    collection = Collection(pk=11)
+    collection.title = "Makeup"
     collection.featured_product = Product(pk=1)
     collection.save()
 
-    collection = Collection(title="Jewelry", featured_product_id=2)
+    collection = Collection(pk=12)
+    collection.featured_product = Product(pk=2)
+    # sets title to "", causes data loss
     collection.save()
 
-    collection = Collection.objects.create(title="Games",featured_product_id=3)
+    collection = Collection.objects.get(pk=13)
+    collection.featured_product = Product(pk=3)
+    # doesnt set title to ""
+    collection.save()
+
+    collection = Collection.objects.filter(pk=14).update(featured_product_id=4)
 
     return render(request, "home.html", {"name": "Nina"})

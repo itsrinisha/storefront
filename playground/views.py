c3d4574493c52b2c4cf4cc95c920c45c7a630bfb
itsrinisha <itsrinisha@gmail.com>
2023-02-12 07:49:14 +0545

    Q Objects

--- playground/views.py
+++ playground/views.py
@@ -1,29 +1,17 @@
 from django.shortcuts import render
+from django.db.models import Q
 from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product
 
 
 def greeting(request):
-    queryset = Product.objects.filter(unit_price__gt=20)
-    queryset = Product.objects.filter(unit_price__lt=20)
-    queryset = Product.objects.filter(unit_price__gte=20)
-    queryset = Product.objects.filter(unit_price__lte=20)
-    queryset = Product.objects.filter(unit_price__range=(10, 50))
+    queryset = Product.objects.filter(inventory__gt=10, unit_price__gt=20)
+    queryset = Product.objects.filter(inventory__gt=10).filter(unit_price__gt=20)
 
-    queryset = Product.objects.filter(collection__id=1)
-    queryset = Product.objects.filter(collection__id__range=(1, 2, 3))
-    queryset = Product.objects.filter(collection__title="Beauty")
+    queryset = Product.objects.filter(Q(inventory__gt=10) & Q(unit_price__gt=10))
+    queryset = Product.objects.filter(Q(inventory__gt=10) | Q(unit_price__gt=10))
+    queryset = Product.objects.filter(Q(inventory__gt=10) & ~Q(unit_price__gt=10))
 
-    queryset = Product.objects.filter(title__contains="coffee")
-    queryset = Product.objects.filter(title__icontains="coffee")
-    queryset = Product.objects.filter(title__startswith="coffee")
-    queryset = Product.objects.filter(title__istartswith="coffee")
-    queryset = Product.objects.filter(title__endswith="coffee")
-    queryset = Product.objects.filter(title__iendswith="coffee")
-
-    queryset = Product.objects.filter(last_update__year=2000)
-
-    queryset = Product.objects.filter(description__isnull=True)
     list(queryset)
 
     return render(request, "home.html", {"name": "Nina"})

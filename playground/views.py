0bbbfe387ea3d572672bc914f5a5b4572be08894
itsrinisha <itsrinisha@gmail.com>
2023-02-12 07:56:31 +0545

    Sorting Data

--- playground/views.py
+++ playground/views.py
@@ -5,8 +5,20 @@ from store.models import Product
 
 
 def greeting(request):
-    queryset = Product.objects.filter(inventory=F("unit_price"))
-    queryset = Product.objects.filter(inventory=F("collection__id"))
+    queryset = Product.objects.order_by("title")
+    queryset = Product.objects.order_by("-title")
+
+    queryset = Product.objects.order_by("unit_price", "title")
+    queryset = Product.objects.order_by("unit_price", "-title")
+    queryset = Product.objects.order_by("-unit_price", "title")
+    queryset = Product.objects.order_by("-unit_price", "-title")
+
+    queryset = Product.objects.order_by("title").reverse()
+    queryset = Product.objects.filter(collection__id=1).order_by("unit_price")
     list(queryset)
+
+    product = Product.objects.order_by("unit_price")[0]
+    product = Product.objects.earliest("unit_price")
+    product = Product.objects.latest("unit_price")
 
     return render(request, "home.html", {"name": "Nina"})

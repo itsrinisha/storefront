03d2c85c957c6f53c87fb7dba2890d43459e4331
itsrinisha <itsrinisha@gmail.com>
2023-02-12 07:51:35 +0545

    F Objects

--- playground/views.py
+++ playground/views.py
@@ -1,17 +1,12 @@
 from django.shortcuts import render
-from django.db.models import Q
+from django.db.models import F
 from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product
 
 
 def greeting(request):
-    queryset = Product.objects.filter(inventory__gt=10, unit_price__gt=20)
-    queryset = Product.objects.filter(inventory__gt=10).filter(unit_price__gt=20)
-
-    queryset = Product.objects.filter(Q(inventory__gt=10) & Q(unit_price__gt=10))
-    queryset = Product.objects.filter(Q(inventory__gt=10) | Q(unit_price__gt=10))
-    queryset = Product.objects.filter(Q(inventory__gt=10) & ~Q(unit_price__gt=10))
-
+    queryset = Product.objects.filter(inventory=F("unit_price"))
+    queryset = Product.objects.filter(inventory=F("collection__id"))
     list(queryset)
 
     return render(request, "home.html", {"name": "Nina"})

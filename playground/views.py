04f23c2ae70ad0a20979c15982132de08062143f
itsrinisha <itsrinisha@gmail.com>
2023-02-12 09:37:09 +0545

    Creating Objects

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
-from store.models import Product
-from tags.models import TaggedItem
+from store.models import Collection, Product
 
 
 def greeting(request):
-    queryset = TaggedItem.objects.get_tags_for(Product, 10)
-    list(queryset)
+    collection = Collection()
+    collection.title = "Beauty"
+    collection.featured_product = Product(pk=1)
+    collection.save()
+
+    collection = Collection(title="Jewelry", featured_product_id=2)
+    collection.save()
+
+    collection = Collection.objects.create(title="Games",featured_product_id=3)
+
     return render(request, "home.html", {"name": "Nina"})

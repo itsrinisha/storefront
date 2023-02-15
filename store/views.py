603942e247d966f88d343ea85c7f9c8bee9331f7
itsrinisha <itsrinisha@gmail.com>
2023-02-15 12:40:25 +0545

    Creating an object

--- store/views.py
+++ store/views.py
@@ -5,11 +5,19 @@ from .models import Product, Collection
 from .serializers import ProductSerializer, CollectionSerializer
 
 
-@api_view()
+@api_view(["GET", "POST"])
 def product_list(request):
-    queryset = Product.objects.select_related("collection").all()
-    serializer = ProductSerializer(queryset, many=True, context={"request": request})
-    return Response(serializer.data)
+    if request.method == "GET":
+        queryset = Product.objects.select_related("collection").all()
+        serializer = ProductSerializer(
+            queryset, many=True, context={"request": request}
+        )
+        return Response(serializer.data)
+    elif request.method == "POST":
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response("OK")
 
 
 @api_view()
